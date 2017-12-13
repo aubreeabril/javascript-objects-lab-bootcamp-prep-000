cf707c35aad1e3f9f982afe45afbe951b4a958ca
Aubree Abril <aubree.abril@gmail.com>
2017-12-13 14:52:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var recipes = {
-  prop:'1'
-}
+var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value
-  return recipes
+  return Object.assign({}, object, { [key]: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value
-  return recipes
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
 }
