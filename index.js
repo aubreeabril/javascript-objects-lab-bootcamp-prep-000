03f915b9538173acad981de14472c5d7c23cb46b
Aubree Abril <aubree.abril@gmail.com>
2017-12-13 14:50:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {
+  prop:'1'
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes[key] = value
+  return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes[key] = value
+  return recipes
+}
